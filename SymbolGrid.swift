cdc8182d21b9e0ceb49af38003eaf11b41d09a9a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-04-12 16:13:51 -0300

    Building the Navigation Grid

--- SymbolGrid.swift
+++ SymbolGrid.swift
@@ -1,12 +1,66 @@
 import SwiftUI
 
-struct                 SymbolGrid: View {
+struct SymbolGrid: View {
+    @State private var symbolNames = [
+        "tshirt",
+        "eyes",
+        "eyebrow",
+        "nose",
+        "mustache",
+        "mouth",
+        "eyeglasses",
+        "facemask",
+        "brain.head.profile",
+        "brain",
+        "icloud",
+        "theatermasks.fill",
+    ]
+    
+    var columnsText: String {
+        numColumns > 1 ? "\(numColumns) Columns" : "1 Column"
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            ScrollView {
+                LazyVGrid(columns: gridColumns) {
+                    ForEach(symbolNames, id: \.self) { name in
+                        NavigationLink(destination: ItemDetail(symbolName: name)) {
+                            ZStack(alignment: .topTrailing) {
+                                Image(systemName: name)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .symbolRenderingMode(.hierarchical)
+                                    .foregroundColor(.accentColor)
+                                    .ignoresSafeArea(.container, edges: .bottom)
+                                    .cornerRadius(8)
+                                if isEditing {
+                                    Button {
+                                        guard let index = symbolNames.firstIndex(of: name) else { return }
+                                        withAnimation {
+                                            _ = symbolNames.remove(at: index)
+                                        }
+                                    } label: {
+                                        Image(systemName: "xmark.square.fill")
+                                            .font(Font.title)
+                                            .symbolRenderingMode(.palette)
+                                            .foregroundColor(.white, Color.red)
+                                    }
+                                    .offset(x: 7, y: -7)
+                                }
+                            }
+                            .padding()
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("My Symbols")
+        .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: #isAddingSymbol, onDismiss: addSymbol) {
+            SymbolPicker(name: $selectedSymbolName)
         }
     }
 }
